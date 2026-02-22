51c2088bc7cadb1303d795e2671a6dc01f1dedc0
Felix5124 <khoanguyen3633@gmail.com>
2026-02-23 01:10:36 +0700

    Initial commit

---
+++ src/test/java/com/bidvibe/bidvibeapispring/BidvibeApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidvibe.bidvibeapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidvibeApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

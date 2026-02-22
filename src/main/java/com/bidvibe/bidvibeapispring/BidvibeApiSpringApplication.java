f99483d1312f8f1e1930484dcaf87cfdb31ae109
Felix5124 <khoanguyen3633@gmail.com>
2026-02-23 01:10:36 +0700

    Initial commit

---
+++ src/main/java/com/bidvibe/bidvibeapispring/BidvibeApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bidvibe.bidvibeapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidvibeApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidvibeApiSpringApplication.class, args);
+    }
+
+}
